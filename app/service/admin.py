1fe2b0d0e28f90587bb90e7461b796b34c0597b6
nazlinurakyol <nazlinurakyol@gmail.com>
2023-06-19 21:40:09 +0200

    add homepage

--- app/service/admin.py
+++ app/service/admin.py
@@ -1,12 +1,15 @@
-from flask_admin import Admin
+import requests
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from wtforms import SelectMultipleField
 from wtforms.widgets import ListWidget, CheckboxInput
 from app.model import tag, video, comedian, youtubeLink, db
+from flask_admin import BaseView, expose
+
+
 
 class VideoModelView(ModelView):
     form_excluded_columns = ('creation_date',)
-
     form_columns = ('title', 'link', 'description', 'is_active', 'tags',)
 
     def create_form(self, obj=None):
@@ -21,13 +24,35 @@ class VideoModelView(ModelView):
     def on_model_change(self, form, model, is_created):
         model.tags = tag.Tag.query.filter(tag.Tag.id.in_(form.tags.data)).all()
 
+
+class MyHomeView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        response = requests.get('http://localhost:5000/api/stat')
+        result = response.json()
+        return self.render('admin/index.html', result=result)
+
+    default_view = 'index'
+
+
+
 admin = None
 
 def start_admin(app):
     global admin
-    admin = Admin(app)
+    admin = Admin(app, index_view=MyHomeView())
     admin.add_view(VideoModelView(video.Video, db.session))
     admin.add_view(ModelView(comedian.Comedian, db.session))
     admin.add_view(ModelView(tag.Tag, db.session))
     admin.add_view(ModelView(youtubeLink.YoutubeLink, db.session))
 
+
+
+#class CustomAdminIndexView(AdminIndexView):
+#    def __init__(self, *args, **kwargs):
+#        super(AdminIndexView, self).__init__(*args, **kwargs)
+#
+#    def render(self, template, **kwargs):
+#        response = requests.get('http://localhost:5000/api/stat')
+#        result = response.json()
+#        return super(CustomAdminIndexView, self).render_template(template, result=result, **kwargs)
