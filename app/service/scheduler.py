2e77c7d5059d2bd7578cc6466ec3dfb6ec3680bc
nazlinurakyol <nazlinurakyol@gmail.com>
2023-06-21 00:18:49 +0200

    change scheduler

--- app/service/scheduler.py
+++ app/service/scheduler.py
@@ -1,3 +1,4 @@
+from datetime import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.service.videos import addVideoAuto
 from app.service.youtube import runYoutubeAuto
@@ -7,7 +8,9 @@ scheduler = None
 def start_scheduler():
     global scheduler
     scheduler = BackgroundScheduler()
+    target_time = time(hour=17, minute=0, second=0)
 
-    scheduler.add_job(addVideoAuto, 'interval', hours=1, id='my_new_video')
-    scheduler.add_job(runYoutubeAuto, 'interval', hours=1, id='my_new_link')
+    scheduler.add_job(addVideoAuto, 'interval', hours=12, id='my_new_video')
+    scheduler.add_job(runYoutubeAuto, 'cron', day_of_week ='*', hour=target_time.hour, minute=target_time.minute, second=target_time.second, id='my_new_link')
+
     scheduler.start()
