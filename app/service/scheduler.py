774cf9abc93624cfb4ad0d97fde2d048b1eaec9b
nazlinurakyol <nazlinurakyol@gmail.com>
2023-06-21 20:14:44 +0200

    minor fix

--- app/service/scheduler.py
+++ app/service/scheduler.py
@@ -9,7 +9,7 @@ def start_scheduler():
     global scheduler
     scheduler = BackgroundScheduler()
     runYoutubeAuto_time = time(hour=17, minute=0, second=0)
-    addVideoAuto_time = time(hour=17, minute=10, second=0)
+    addVideoAuto_time = time(hour=18, minute=0, second=0)
 
     scheduler.add_job(runYoutubeAuto, 'cron', day_of_week ='*', hour=runYoutubeAuto_time.hour, minute=runYoutubeAuto_time.minute, second=runYoutubeAuto_time.second, id='my_new_link')
     scheduler.add_job(addVideoAuto, 'cron', day_of_week ='*', hour=addVideoAuto_time.hour, minute= addVideoAuto_time.minute, second= addVideoAuto_time.second, id='my_new_video')
