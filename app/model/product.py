f3895b33505c020ce5c7feb60196609f66aa285a
nazlinurakyol <nazlinurakyol@gmail.com>
2023-06-23 15:31:52 +0200

    fix on product.py

--- app/model/product.py
+++ app/model/product.py
@@ -6,15 +6,15 @@ class Product(db.Model):
     comedian_id = db.Column(db.String(36), db.ForeignKey("comedian.id"))
     title = db.Column(db.String(250), unique=False, nullable=False)
     product_link = db.Column(db.String(250), unique=True,  nullable=False)
-    image_link = db.Column(db.String(250), unique=False,  nullable=False)
+    image_src = db.Column(db.String(250), unique=False,  nullable=False)
 
-    def __init__(self, id, title, product_link, image_link ):
+    def __init__(self, id, title, product_link, image_src ):
         self.id = id
         self.title = title
         self.product_link = product_link
-        self.image_link = image_link
+        self.image_src = image_src
 
     def to_dict(self):
-        return {"id": self.id, "title": self.title, "product_link": self.product_link, "image_link" : self.image_link}
+        return {"id": self.id, "title": self.title, "product_link": self.product_link, "image_src" : self.image_src}
 
 
